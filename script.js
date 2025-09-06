dc4454c6fd2e2ba0aa02f617dc991c42c49502d3
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-09-05 23:33:32 -0400

    Create basic 4 function calculator

--- script.js
+++ script.js
@@ -1,19 +1,93 @@
-function add(a, b)
+let numOne = "";
+let numTwo = "";
+let operator = null;
+
+function setNum(num)
 {
-    return a+b;
+    let display = document.querySelector("#display");
+    if(numOne == "")
+    {
+        numOne = num;
+        display.textContent = numOne;
+    }
+    else if(operator == null && numOne.length<39)
+    {
+        numOne += num;
+        display.textContent = numOne;
+    }
+    else if(numTwo == "" && operator != null)
+    {
+        numTwo = num;
+        display.textContent = numTwo;
+    }
+    else if(numTwo != "" && numTwo.length<39)
+    {
+        numTwo += num;
+        display.textContent = numTwo;
+    }
+
+    console.log("numOne: " + numOne);
+    console.log("numTwo: " + numTwo);
+    console.log("ops: " + operator);
 }
 
-function subtract(a, b)
+function setOps(ops)
 {
-    return a-b;
+    if(numOne == "" && numTwo == "")
+        return;
+    
+    if(numOne != "" && numTwo != "")
+        operate();
+
+    if(numOne == "" && document.querySelector("#display").textContent != "Divide by zero")
+        numOne = document.querySelector("#display").textContent;
+    
+    switch(ops)
+    {
+        case "add":
+            operator = function(a, b) {
+                return Number(a)+Number(b);
+            }
+            break;
+        case "subtract":
+            operator = function(a, b) {
+                return a-b;
+            }
+            break;
+        case "multiply":
+            operator = function(a, b) {
+                return a*b;
+            }
+            break;
+        case "divide":
+            operator = function(a, b) {
+                if(b == 0)
+                    return "Divide by zero"
+
+                return a/b;
+            }
+            break;
+    }
 }
 
-function multiply(a, b)
+function clearAll()
 {
-    return a*b;
+    numOne = "";
+    numTwo = "";
+    operator = null;
+
+    document.querySelector("#display").textContent = "";
 }
 
-function divide(a, b)
+function operate()
 {
-    return a/b;
+    if(operator == null || numOne == "" || numTwo == "")
+        return;
+
+    let result = operator(numOne, numTwo);
+    document.querySelector("#display").textContent = result;
+
+    numOne = "";
+    numTwo = "";
+    operator = null;
 }
